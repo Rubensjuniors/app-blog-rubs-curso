b647f6510132be328244995663b6d7bfdd06adbd
rubens.marques <rubens.marques@hotmart.com>
2024-08-10 15:54:39 -0300

    feat: added configs tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 
 import { allPosts } from 'contentlayer/generated'
+
 import { Mdx } from '@/components'
+import Button from '@/components/Button'
 
 export default function Home() {
   const posts = allPosts.map(post => post)
@@ -9,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 dangerouslySetInnerHTML={{ __html: posts[0]?.title }} />
-
+      <Button />
       <Image src={posts[0].image} width={700} height={700} alt="" />
 
       <Mdx code={posts[0].body.code} />
